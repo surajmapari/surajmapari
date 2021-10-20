a82646bb28aab77072ebab1911776e997a183fff
SURAJ MAPARI <57449206+surajmapari@users.noreply.github.com>
2021-10-20 16:24:21 +0530

    Update Write a Python program to store marks scored in subject “Fundamental of Data # Structure” by N students in the class.py

--- CodeFiles/python/Write a Python program to store marks scored in subject “Fundamental of Data # Structure” by N students in the class.py
+++ CodeFiles/python/Write a Python program to store marks scored in subject “Fundamental of Data # Structure” by N students in the class.py
@@ -1,9 +1,9 @@
-# Write a Python program to store marks scored in subject “Fundamental of Data
-# Structure” by N students in the class. Write functions to compute following:
-# a)The average score of class
-# b)Highest score and lowest score of class
-# c)Count of students who were absent for the test
-# d)Display mark with highest frequency
+"""Write a Python program to store marks scored in subject “Fundamental of Data
+Structure” by N students in the class. Write functions to compute following:
+a)The average score of class
+b)Highest score and lowest score of class
+c)Count of students who were absent for the test
+d)Display mark with highest frequency."""
 
 import sys
 
